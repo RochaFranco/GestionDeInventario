67a285593137ff41c7bd5f89b50571ca1db4581a
RochaFranco <rochafrancoagustin@hotmail.com>
2023-10-14 19:25:44 -0300

    Ahora se pueden elimniar los productos desde la ventana Eliminar producto

--- GestionDeInventarioWPF/GestionDeInventarioWPF/Screens/EliminarProductoPagina.xaml.cs
+++ GestionDeInventarioWPF/GestionDeInventarioWPF/Screens/EliminarProductoPagina.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using GestionDeInventarioWPF.Clases;
+using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,6 +22,9 @@ namespace GestionDeInventarioWPF.Screens
     /// </summary>
     public partial class EliminarProductoPagina : Page
     {
+
+        DBManager dbManager = new DBManager();
+
         public EliminarProductoPagina()
         {
             InitializeComponent();
@@ -27,6 +32,30 @@ namespace GestionDeInventarioWPF.Screens
 
         private void EliminarProducto_Button_Click(object sender, RoutedEventArgs e)
         {
+
+
+            try
+            {
+                // Código de inserción aquí
+                string query = "DELETE FROM Producto WHERE id = @id";
+                using (MySqlCommand cmd = new MySqlCommand(query, dbManager.GetConnection()))
+                {
+
+                    cmd.Parameters.AddWithValue("@id", IdProducto_TextBox.Text);
+
+                    dbManager.OpenConnection();
+                    cmd.ExecuteNonQuery();
+                    dbManager.CloseConnection();
+
+                    MessageBox.Show("Producto Eliminado exitosamente", "Éxito", MessageBoxButton.OK, MessageBoxImage.Information);
+
+                }
+            }
+            catch (Exception ex)
+            {
+                // Manejo de la excepción
+                MessageBox.Show("Error al eliminar el producto: " + ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
 
         }
     }
